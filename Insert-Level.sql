6148da01f4f81699942e7682e7dd2237b5c02229
Laksmi Setiawati <35263876+laksmisetiawati@users.noreply.github.com>
2025-03-19 14:05:48 +0700

    Create Insert-Level.sql

---
+++ Insert-Level.sql
@@ -0,0 +1,6 @@
+INSERT INTO Level (idLevel, namaLevel, keterangan)
+VALUES
+(1, 'Staff', 'Hanya bisa mengisikan data diri'),
+(2, 'Supervisor', 'Dapat memberikan nilai Attitude, Layanan, Ketelitian'),
+(3, 'Manager', 'Dapat memberikan nilai KPI dan Presensi serta approve penilaian'),
+(4, 'Direktur', 'Menghitung nilai, membuat laporan akhir, dan memberikan reward');
