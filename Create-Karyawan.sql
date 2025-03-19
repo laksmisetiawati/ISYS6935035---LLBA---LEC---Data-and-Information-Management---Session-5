c7ebd2e0279ac0239a332e617636e2b5d605c203
Laksmi Setiawati <35263876+laksmisetiawati@users.noreply.github.com>
2025-03-19 14:07:30 +0700

    Create Create-Karyawan.sql

---
+++ Create-Karyawan.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Karyawan(
+    idKaryawan INT NOT NULL,
+    namaKaryawan VARCHAR(255) NOT NULL,
+    alamatKaryawan TEXT NOT NULL,
+    handphoneKaryawan CHAR(16) NOT NULL,
+    pendidikanKaryawan VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password CHAR(32) NOT NULL,
+    idLevel INT NOT NULL,
+    proses VARCHAR(255),
+    PRIMARY KEY (idKaryawan),
+    FOREIGN KEY (idLevel) REFERENCES Level(idLevel)
+);
