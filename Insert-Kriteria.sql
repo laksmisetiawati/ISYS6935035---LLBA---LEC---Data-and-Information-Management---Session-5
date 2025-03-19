1a4e3dbc9205ae076f220cd738bd049cebb04932
Laksmi Setiawati <35263876+laksmisetiawati@users.noreply.github.com>
2025-03-19 14:05:03 +0700

    Create Insert-Kriteria.sql

---
+++ Insert-Kriteria.sql
@@ -0,0 +1,7 @@
+INSERT INTO Kriteria (idKriteria, namaKriteria, bobot, jenis, keterangan)
+VALUES
+(1, 'KPI', 35, 'Benefit', 'Key Performance Indicator'),
+(2, 'Attitude', 20, 'Benefit', 'Sikap dalam bekerja'),
+(3, 'Layanan', 15, 'Benefit', 'Pelayanan terhadap pelanggan'),
+(4, 'Presensi', 10, 'Benefit', 'Kehadiran dalam bekerja'),
+(5, 'Ketelitian', 20, 'Benefit', 'Tingkat ketelitian dalam pekerjaan');
