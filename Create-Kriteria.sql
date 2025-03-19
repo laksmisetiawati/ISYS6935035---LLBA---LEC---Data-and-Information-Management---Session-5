c03444f9934f4549860e9a9589e5b77fc7100bf3
Laksmi Setiawati <35263876+laksmisetiawati@users.noreply.github.com>
2025-03-19 14:10:40 +0700

    Create Create-Kriteria.sql

---
+++ Create-Kriteria.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Kriteria(
+    idKriteria INT NOT NULL,
+    namaKriteria VARCHAR(255) NOT NULL,
+    jenis VARCHAR(255) NOT NULL,
+    bobot INT NOT NULL,
+    keterangan VARCHAR(255),
+    PRIMARY KEY (idKriteria)
+);
