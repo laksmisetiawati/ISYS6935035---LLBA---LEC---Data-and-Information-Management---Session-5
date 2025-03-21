58e3708a3ba510b3800c92d439e42493d72ed954
Laksmi Setiawati <35263876+laksmisetiawati@users.noreply.github.com>
2025-03-21 08:46:11 +0700

    Update Create-Level.sql

--- Create-Level.sql
+++ Create-Level.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Level(
     idLevel INT NOT NULL,
     namaLevel VARCHAR(255) NOT NULL,
-    keterangan VARCHAR(255),
-    PRIMARY KEY idLevel
+    keterangan VARCHAR(255) NULL,
+    PRIMARY KEY (idLevel)
 );
